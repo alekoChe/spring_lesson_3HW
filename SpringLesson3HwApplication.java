f9369fa5cb9509344e2dae193c47f33d2bc15a6e
alekoChe <aleko2117bis@gmail.com>
2022-04-22 14:27:29 +0300

    Initial commit

---
+++ SpringLesson3HwApplication.java
@@ -0,0 +1,13 @@
+package ru.gbAleko.spring_lesson_3HW;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringLesson3HwApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringLesson3HwApplication.class, args);
+	}
+
+}
