f2c9034107fdecc1cbf3c63df33f862666be9eb4
alekoChe <aleko2117bis@gmail.com>
2022-04-22 14:38:59 +0300

    add Repository.java and ProductInMemoryRepository.java

---
+++ repository/ProductInMemoryRepository.java
@@ -0,0 +1,34 @@
+package ru.gbAleko.spring_lesson_3HW.repository;
+
+import org.springframework.context.annotation.Primary;
+import org.springframework.stereotype.Component;
+import ru.gbAleko.spring_lesson_3HW.model.Product;
+
+import javax.annotation.PostConstruct;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+@Component
+@Primary
+public class ProductInMemoryRepository implements Repository{
+
+    private List<Product> productList;
+
+    @PostConstruct
+    public void init(){
+        productList = new ArrayList<>(Arrays.asList(
+                new Product("Milk", 1L, 10),
+                new Product("Bread", 2L, 15),
+                new Product("Apple", 3L, 20),
+                new Product("Orange", 4L, 2),
+                new Product("Butter", 5L, 25)
+        ));
+    }
+
+    @Override
+    public Product findById(Long id){
+        return productList.stream().filter(p -> p.getId().equals(id)).findFirst().orElseThrow(RuntimeException::new);
+    }
+
+}
